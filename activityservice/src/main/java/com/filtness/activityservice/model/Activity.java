c4cec3d18bb31fa829bad780970757c8e3cf503c
imam-007 <imimam8409@gmail.com>
2025-11-11 23:44:52 +0530

    Implement activity model

--- activityservice/src/main/java/com/filtness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/filtness/activityservice/model/Activity.java
@@ -1,9 +1,24 @@
 package com.filtness.activityservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@Entity
+@Table(name = "Activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Activity {
+    @Id
     private String id;
     private String userId;
     private ActivityType type;
