01581de96bf6e8ede27f56e0569c43fe002e8282
imam-007 <imimam8409@gmail.com>
2025-12-04 23:43:39 +0530

    Building the Frontend Form on Dashboard

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { BrowserRouter as Router } from 'react-router'
-import './App.css'
-import { Button } from '@mui/material'
+import { BrowserRouter as Router , Navigate, Route, Routes, useLocation} from 'react-router'
+// import './App.css'
+import { Box, Button } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from 'react-oauth2-code-pkce'
 import { useDispatch } from 'react-redux'
 import { setCredential } from './store/authSlice'
+import ActivityForm from './components/ActivityForm'
+import ActivityList from './components/ActivityList'
+import ActivityDetails from './components/ActivityDetails'
+
+const ActivityPage = () => {
+  return(
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      <ActivityForm onActivitiesAdded = {() => window.Location.reload()} />
+      <ActivityList />
+    </Box>
+  )
+}
 
 function App() {
 
@@ -29,10 +41,14 @@ function App() {
         }}
         >Login</Button> 
       ) : (
-        <div>
-          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
-          <pre>{JSON.stringify(token, null, 2)}</pre>
-        </div>
+        <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+          <Routes>
+            <Route path='/activities' element={<ActivityPage />}/>
+            <Route path='/activities/:id' element={<ActivityDetails />}/>
+
+            <Route path='/' element={token ? <Navigate to="/activities" replace /> :<div> Welcome! Please Login </div>} />
+          </Routes>
+        </Box>
       )}
     </Router>
   )
