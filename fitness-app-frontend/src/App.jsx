dac90568528b2a1bd70a8fde517f3540a62b7cb8
imam-007 <imimam8409@gmail.com>
2025-12-05 17:50:27 +0530

    Building Activity Detail Page

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router , Navigate, Route, Routes, useLocation} from 'react-router'
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router'
 // import './App.css'
 import { Box, Button } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
@@ -10,43 +16,55 @@ import ActivityList from './components/ActivityList'
 import ActivityDetails from './components/ActivityDetails'
 
 const ActivityPage = () => {
-  return(
+  return (
     <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
-      <ActivityForm onActivitiesAdded = {() => window.Location.reload()} />
+      <ActivityForm onActivitiesAdded={() => window.Location.reload()} />
       <ActivityList />
     </Box>
   )
 }
 
 function App() {
-
-  const {token, tokenData, logIn, logOut, isAuthenticated } = useContext(AuthContext)
-  const dispatch = useDispatch();
+  const { token, tokenData, logIn, logOut, isAuthenticated } =
+    useContext(AuthContext)
+  const dispatch = useDispatch()
   const [authReady, setAuthReady] = useState(false)
 
   useEffect(() => {
     if (token) {
-      dispatch(setCredential({token, user: tokenData}));
+      dispatch(setCredential({ token, user: tokenData }))
       setAuthReady
     }
-
-  }, [token, tokenData, dispatch]);
+  }, [token, tokenData, dispatch])
 
   return (
     <Router>
       {!token ? (
-          <Button variant='contained' color='#dc004e'
-        onClick = {() =>{
-          logIn();
-        }}
-        >Login</Button> 
+        <Button
+          variant="contained"
+          color="#dc004e"
+          onClick={() => {
+            logIn()
+          }}
+        >
+          Login
+        </Button>
       ) : (
         <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
           <Routes>
-            <Route path='/activities' element={<ActivityPage />}/>
-            <Route path='/activities/:id' element={<ActivityDetails />}/>
+            <Route path="/activities" element={<ActivityPage />} />
+            <Route path="/activities/:id" element={<ActivityDetails />} />
 
-            <Route path='/' element={token ? <Navigate to="/activities" replace /> :<div> Welcome! Please Login </div>} />
+            <Route
+              path="/"
+              element={
+                token ? (
+                  <Navigate to="/activities" replace />
+                ) : (
+                  <div> Welcome! Please Login </div>
+                )
+              }
+            />
           </Routes>
         </Box>
       )}
